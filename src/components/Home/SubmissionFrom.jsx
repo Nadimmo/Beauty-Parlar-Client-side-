5ae48e30d9ca4e773e659ffc94803e41ca94e8ba
nadimmo <nadimmostafa334@gmail.com>
2024-12-22 20:58:33 +0600

    coustomize footer and improve style

--- src/components/Home/SubmissionFrom.jsx
+++ src/components/Home/SubmissionFrom.jsx
@@ -25,7 +25,7 @@ const SubmissionForm = () => {
 
     return (
         <div className="mt-28 p-10 bg-[#fff8f5] shadow-lg rounded-lg">
-            <h2 className="text-2xl font-bold mb-4 text-center">Submission Form</h2>
+            <h2 className="text-2xl font-bold mb-4 text-center text-[#F63E7B]">Submission Form</h2>
             <form onSubmit={handleSubmit} className="space-y-4 lg:w-1/2 mx-auto">
                 <div className="space-y-4">
                     <div className="grid grid-cols-2 gap-4">
@@ -105,7 +105,7 @@ const SubmissionForm = () => {
                     <div className="text-center">
                         <button
                             type="submit"
-                            className="w-full py-2 px-4 bg-indigo-600 text-white font-semibold rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                            className="w-52 py-2 px-4 bg-[#F63E7B] text-white font-semibold rounded-md hover:bg-violet-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
                         >
                             Send Message
                         </button>
