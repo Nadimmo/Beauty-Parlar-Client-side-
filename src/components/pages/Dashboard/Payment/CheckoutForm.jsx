75a7c207c04ef56f55212759e0e1ab638153a7fe
nadimmo <nadimmostafa334@gmail.com>
2024-12-29 14:47:33 +0600

    create payment intent

--- src/components/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/components/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,23 +1,45 @@
-import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
-import React from 'react'
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { AuthContext } from "../../../AuthProvider/AuthProvider";
+import useBooking from "../../../Hooks/useBooking";
+import useContact from "../../../Hooks/useContact";
+import { useEffect, useState } from "react";
+import useAxiosPublic from './../../../Hooks/useAxiosPublic';
+
 
 const CheckoutForm = () => {
+  const axiosPublic= useAxiosPublic()
+  const {user} = useContact(AuthContext)
+  const { bookings } = useBooking()
   const stripe = useStripe();
   const elements = useElements();
+  const [clientSecret, setClientSecret] = useState("");
+  const [cartError, setCartError] = useState("");
+
+  const totalPrice = bookings.reduce(
+    (acc, booking) => acc + parseInt(booking.price),
+    0
+  );
+
+  //create payment-intent
+ 
+  useEffect(() => {
+    if (totalPrice > 0) {
+      axiosPublic
+        .post("/create-payment-intent", { price: totalPrice })
+        .then((res) => {
+          setClientSecret(res.data.clientSecret);
+        });
+    }
+  }, [axiosPublic, totalPrice]);
+
 
   const handleSubmit = async (event) => {
     // Block native form submission.
     event.preventDefault();
 
     if (!stripe || !elements) {
-      // Stripe.js has not loaded yet. Make sure to disable
-      // form submission until Stripe.js has loaded.
       return;
     }
-
-    // Get a reference to a mounted CardElement. Elements knows how
-    // to find your CardElement because there can only ever be one of
-    // each type of element.
     const card = elements.getElement(CardElement);
 
     if (card == null) {
@@ -31,36 +53,54 @@ const CheckoutForm = () => {
     });
     console.log(paymentMethod)
     if (error) {
-      console.log('[error]', error);
+      setCartError(error.message);
     } else {
-      console.log('[PaymentMethod]', paymentMethod);
+      setCartError("");
     }
+
+    //create confirm payment intent
+    const { paymentIntent, error: cartError} = await stripe.confirmCardPayment(
+      clientSecret,
+      {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: user?.displayName || "anonymous",
+            email: user?.email || "anonymous",
+          },
+        },
+      }
+    );
+    console.log(paymentIntent)
+    console.log(cartError)
   };
 
 
-
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
+    <div>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: '16px',
+                color: '#424770',
+                '::placeholder': {
+                  color: '#aab7c4',
+                },
+              },
+              invalid: {
+                color: '#9e2146',
               },
             },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
+          }}
+        />
+        <button type="submit" disabled={!stripe || !clientSecret}>
+          Pay
+        </button>
+      </form>
+    </div>
+
   )
 }
 
